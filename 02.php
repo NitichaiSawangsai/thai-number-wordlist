e21c67a54f317b79c82621f74cfad7fd7bedbcb7
NitichaiSawangsai <40546651+NitichaiSawangsai@users.noreply.github.com>
2025-09-01 09:31:52 +0700

    Update 02.php

--- 02.php
+++ 02.php
@@ -1,5 +1,11 @@
 <?php
+$filename = "02.txt";
+$handle = fopen($filename, "w");
 
 for ($i = 0; $i < 10000000; $i++) {
-  echo '02' . str_pad($i, 7, STR_PAD_LEFT) . "\n";
+    $number = "02" . str_pad((string)$i, 7, "0", STR_PAD_LEFT) . "\n";
+    fwrite($handle, $number);
 }
+
+fclose($handle);
+echo "สร้างไฟล์ $filename เรียบร้อยแล้ว\n";
